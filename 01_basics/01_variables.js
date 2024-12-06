36d1b1b2b9c12d4286375996fd569995923aa01e
Nitin <152731976+corenitin@users.noreply.github.com>
2024-12-06 17:46:30 +0000

    add 01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 123456
+let accountEmail = "nitin@gmail.com"
+var accountPassword = "282828"
+accountCity ="Ankleshwar"
+let accountState;
+
+// accountId = 234
+accountEmail = "pnitin@1306gmail.com"
+accountPassword = "909090"
+accountCity = "Bharuch"
+
+
+console.log(accountId);
+
+// const can not be after declaring one time change when you run the code 
+
+
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
