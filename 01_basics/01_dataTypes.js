1abcf86e4540dae6a168c09c4d886d1f4e27bedf
Nitin <152731976+corenitin@users.noreply.github.com>
2024-12-06 18:14:20 +0000

    add 01_datatype

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js as newer version
+
+// alert(3+3); we are using node js not browser
+
+let name ="nitin"
+let age = 22;
+let isLoggin = false
+let state = null
+
+// DataTypes in js 
+// number => 2 to power 53 range of number 
+// bigint for more big numbers then number 
+// string => ""
+// Boolean => true/false
+// null=> standalone value (representation of empty value)
+// undefined => (variable is declare but not given value)
+// Symbol => unique 
+// This all are premitive datatypes
+
+
+//object 
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined 
