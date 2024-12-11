c9360bab5e5c1aaf1a3a13fdd07f9d1abf16b295
Nitin <152731976+corenitin@users.noreply.github.com>
2024-12-11 14:06:04 +0000

    add 04_comparison.js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=2);
+// console.log(2<2);
+// console.log(2==2);
+// console.log(2!=2);
+
+// console.log("2">1);
+// console.log("02">1); here automatically JavaScript convert string in to number.
+
+// console.log(null>0);
+// console.log(null==0); this kind of conversation avoid while writing your code because it will create confusion.
+// console.log(null>=0);
+
+// console.log(undefined==0);
+// console.log(undefined>0); this kind of conversation avoid while writing your code because it will create confusion.
+
+// ===
+// console.log("2" === 2); the === check number and their datatypes that is why output is false 
+
+
+
+
