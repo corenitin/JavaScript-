0df51da3414507f03ce10a032d8f945c84776809
Nitin <152731976+corenitin@users.noreply.github.com>
2024-12-11 12:21:33 +0000

    add

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -42,7 +42,25 @@ let str1 = "hello"
 let str2 = " nitin"
 
 let str3 = str1+str2
-console.log(str3);
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1=num2=num3= 2+2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
 
 
 
