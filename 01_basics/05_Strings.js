8758a764e5cc2f06f24ae523e65251897f16005b
Nitin <152731976+corenitin@users.noreply.github.com>
2024-12-13 14:36:23 +0000

    add 05_Strings.js

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,34 @@
+const name = "nitin "
+const repoCount = 9
+
+// console.log(name + repoCount + " value");
+
+// console.log(`Hello my name is ${name} my repocount is ${repoCount}`);
+
+const gameName = new String('ni,tin,np')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+// console.log(gameName.substring(0,3));
+// console.log(gameName.slice(-6,3));
+
+const newString = "  nitin   "
+console.log(newString);
+console.log(newString.trim());
+
+const url = "https://jubilant-palm-tree-4jjvg7v97vvx2vg.github.dev/"
+console.log(url.replace('4','1777'));
+console.log(url.includes('nitin'));
+
+console.log(gameName.split(','));
+
+
+
+
+
