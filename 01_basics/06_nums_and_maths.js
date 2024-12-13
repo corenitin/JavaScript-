7182bcdb80698df6db6f61fda7d3a16a4445611b
Nitin <152731976+corenitin@users.noreply.github.com>
2024-12-13 17:39:19 +0000

    commit

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,18 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(10000660.345)
+// console.log(balance.toString());
+// console.log(balance.toExponential());
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(4));
+// console.log(balance.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++Maths+++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-2));
+console.log(Math.round(2.5789));
+console.log(Math.ceil(2.5789));
+console.log(Math.floor(2.5789));
