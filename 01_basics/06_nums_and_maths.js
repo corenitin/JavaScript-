a40d4a99f71cd4948df52922a957d1fa6d709acf
Nitin <152731976+corenitin@users.noreply.github.com>
2024-12-14 17:00:48 +0000

    add num and math

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -11,8 +11,26 @@
 
 //+++++++++++++++++Maths+++++++++++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-2));
-console.log(Math.round(2.5789));
-console.log(Math.ceil(2.5789));
-console.log(Math.floor(2.5789));
+// console.log(Math);
+// console.log(Math.abs(-2));
+// console.log(Math.round(2.5789));
+// console.log(Math.ceil(2.5789));
+// console.log(Math.floor(2.5789));
+// console.log(Math.max(2, 3,6,7,8));
+// console.log(Math.min(2, 3,6,7,8));
+
+// console.log((Math.random()*100+2));
+// console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1)));
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
+// console.log(Math.floor(Math.random() * (max-min)) + min);
+
+
+
+
