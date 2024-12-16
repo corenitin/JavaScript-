75802764f8b7e2d8ac6b0f6a1a3938e047934d9f
Np <152731976+corenitin@users.noreply.github.com>
2024-12-16 16:10:21 +0000

    add 04_objects.js

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,57 @@
+// const tinderUser = new Object()   #Singleton obj
+const tinderUser = {}   // Non singleton obj
+tinderUser.id = "123abc"
+tinderUser.name = "nitin"
+tinderUser.InLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularuser = {
+    email: "nitin@gmail.com",
+    fullname: {
+        userfullname: {
+            fristname: "nitin",
+            lastname: "parmar"
+        }
+    }
+}
+// console.log(regularuser.fullname.userfullname);
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+// when objects comes from database or users give the data!
+const users = [
+    {
+        id: 1,
+        email: "nitin@gmial.com"
+    },
+    {
+        id: 1,
+        email: "nitin@gmial.com"
+    },
+    {
+        id: 1,
+        email: "nitin@gmial.com"
+    }
+]
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('InLoggedIn'));
+console.log(tinderUser.propertyIsEnumerable('InLoggedIn'));
+
+
+
+
+
+
