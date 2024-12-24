d3afbc9bc0371e9ae5b8ca70e8422b075811c2b6
Np <152731976+corenitin@users.noreply.github.com>
2024-12-24 07:41:35 +0000

    add four four.js

--- 05_iteratinons/four.js
+++ 05_iteratinons/four.js
@@ -6,6 +6,24 @@ const myObject = {
     py : 'python'
 }
 for (const key in myObject) {
-    console.log(key);
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
     
 }
+
+// forin for arrays
+const programming = ['js', 'cpp', 'python', 'java']
+
+for (const key in programming) {
+    // console.log(`${key} : ${programming[key]}`);
+    
+}
+
+// const map = new Map() ### you can not iterate on map.
+// map.set('IN', 'India')
+// map.set('USA', 'Unitd states of America')
+// map.set('AUS', 'Australia')
+
+// for (const key in map) {
+//     console.log(key);  
+// }
+
